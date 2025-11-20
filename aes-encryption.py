9d2df3f84ace73e074ae08c89bc0ebf0d71e41b7
Xavier Tondelier <xtondelier@gmail.com>
2025-11-20 21:39:47 +0000

    Add inverse aes shift rows and mix columns

--- aes-encryption.py
+++ aes-encryption.py
@@ -18,8 +18,7 @@ class AesEncryption:
         inv_num = self._primeModulusHandler.get_denary(self._primeModulusHandler.affine_transformation(A, num, b))
         return self._primeModulusHandler.multiplicative_inverse_in_gf8(inv_num)
 
-    def mix_columns(self, block):
-        matrix = [[2,3,1,1],[1,2,3,1],[1,1,2,3],[3,1,1,2]]
+    def mix_columns(self, block, matrix=[[2,3,1,1],[1,2,3,1],[1,1,2,3],[3,1,1,2]]):
         resultant_block = [[],[],[],[]]
         for column_number in range(4):
             column_data = [row[column_number] for row in block]
@@ -34,11 +33,21 @@ class AesEncryption:
 
         return resultant_block
 
+    def inverse_mix_columns(self, block):
+        return self.mix_columns(block, [[14,11,13,9],[9,14,11,13],[13,9,14,11],[11,13,9,14]])
+
     def shift_rows(self, block):
         resultant_block = [[],[],[],[]]
         for row in range(4):
             for i in range(4):
                 resultant_block[row].append(block[row][(i + row) % 4])
+        return resultant_block
+
+    def inverse_shift_rows(self, block):
+        resultant_block = [[],[],[],[]]
+        for row in range(4):
+            for i in range(4):
+                resultant_block[row].append(block[row][(i - row) % 4])
         return resultant_block
 
     def sub_bytes(self, block):
