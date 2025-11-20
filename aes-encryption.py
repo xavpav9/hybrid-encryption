8080659ad7298065a76d4240974231602a4e65ea
Xavier Tondelier <xtondelier@gmail.com>
2025-11-20 23:36:16 +0000

    Add aes encrypt and decrypt methods
    At this stage, it is quite slow. For future reference, I do not know
    why right now.

--- aes-encryption.py
+++ aes-encryption.py
@@ -1,9 +1,29 @@
+import sys
 from primeModulusHandler import PrimeModulusHandler
 
 class AesEncryption:
-    def __init__(self):
+    def __init__(self, key, encoding): # key must be in ascii
         self._primeModulusHandler= PrimeModulusHandler()
         self.rcon = [1,2,4,8,16,32,64,128,27,54]
+
+        try:
+            if int(encoding) in [8, 16]:
+                self.bits_per_letter = int(encoding)
+                self.letters_per_block = 128 // self.bits_per_letter
+            else:
+                raise Exception()
+        except:
+            raise Exception("Invalid AesEncryption instantiation - pick a value of bits from 8, 16 ( for now )")
+
+        self.set_key(key)
+
+    def set_key(self, key): # key in ascii
+        block = [[],[],[],[]]
+        for o in range(0, 16, 4):
+            for j in range(0, 4):
+                block[o // 4].append(ord(key[o + j]))
+        self.key = block
+
 
     def rijndael_sbox(self, num):
         if num == 0: return 99 # special case
@@ -86,7 +106,95 @@ class AesEncryption:
 
         return resultant_block
 
+    def add_round_key(self, block, round_key):
+        resultant_block = [[],[],[],[]]
+        for row in range(4):
+            for col in range(4):
+                resultant_block[row].append(self._primeModulusHandler.xor(block[row][col], round_key[row][col]))
+        return resultant_block
 
+    def encrypt(self, data):
+        encrypted_data = ""
+        round_keys = []
+
+        block = self.next_key(self.key, [self.rcon[0],0,0,0])
+        round_keys.append(block)
+
+        for i in range(1,10):
+            block = self.next_key(block, [self.rcon[i], 0, 0, 0])
+            round_keys.append(block)
+
+        for i in range(0, len(data), self.letters_per_block):
+            str_data = data[i:i+self.letters_per_block]
+            bin = ""
+            for letter in list(str_data):
+                bin += self._primeModulusHandler.get_bit_pattern(ord(letter), self.bits_per_letter)
+            bin += "0" * (128 - len(bin))
+            block = [[],[],[],[]]
+            for o in range(0, 128, 32):
+                for j in range(0, 32, 8):
+                    block[o // 32].append(self._primeModulusHandler.get_denary(bin[o+j:o+j+8]))
+
+            block = self.add_round_key(block, self.key)
+            for round in range(9):
+                block = self.sub_bytes(block)
+                block = self.shift_rows(block)
+                block = self.mix_columns(block)
+                block = self.add_round_key(block, round_keys[round])
+            block = self.sub_bytes(block)
+            block = self.shift_rows(block)
+            block = self.add_round_key(block, round_keys[9])
+            
+            for row in range(4):
+                for col in range(4):
+                    encrypted_data += chr(block[row][col])
+
+        return encrypted_data
+
+
+    def decrypt(self, data):
+        decrypted_data = ""
+        round_keys = []
+
+        block = self.next_key(self.key, [self.rcon[0],0,0,0])
+        round_keys.append(block)
+
+        for i in range(1,10):
+            block = self.next_key(block, [self.rcon[i], 0, 0, 0])
+            round_keys.append(block)
+
+        for i in range(0, len(data), 16):
+            str_data = data[i:i+16]
+            bin = ""
+            for letter in list(str_data):
+                bin += self._primeModulusHandler.get_bit_pattern(ord(letter))
+            bin += "0" * (128 - len(bin))
+            block = [[],[],[],[]]
+            for o in range(0, 128, 32):
+                for j in range(0, 32, 8):
+                    block[o // 32].append(self._primeModulusHandler.get_denary(bin[o+j:o+j+8]))
+
+            block = self.add_round_key(block, round_keys[9])
+            block = self.inverse_shift_rows(block)
+            block = self.inverse_sub_bytes(block)
+            for round in range(8, -1, -1):
+                block = self.add_round_key(block, round_keys[round])
+                block = self.inverse_mix_columns(block)
+                block = self.inverse_shift_rows(block)
+                block = self.inverse_sub_bytes(block)
+
+            block = self.add_round_key(block, self.key)
+            
+            decrypted_part = ""
+            for row in range(4):
+                for col in range(4):
+                    decrypted_part += self._primeModulusHandler.get_bit_pattern(block[row][col])
+            
+            for o in range(0, 128, self.bits_per_letter):
+                decrypted_data += chr(self._primeModulusHandler.get_denary(decrypted_part[o:o+self.bits_per_letter]))
+
+
+        return decrypted_data
 
 """tests:
 aes = AesEncryption()
@@ -97,4 +205,41 @@ for i in range(10):
     b1 = aes.next_key(b1, [aes.rcon[i], 0, 0, 0])
 
 print(b1)
+
+---------
+
+block = [[int("d4", 16),int("e0", 16),int("b8", 16),int("1e", 16)],[int("bf", 16),int("b4", 16),int("41", 16),int("27", 16)],[int("5d", 16),int("52", 16),int("11", 16),int("98", 16)],[int("30", 16),int("ae", 16),int("f1", 16),int("e5", 16)]]
+print(aes.inverse_mix_columns(aes.mix_columns(block)))
+
+---------
+
+message = ""
+for i in ["32", "88", "31", "e0", "43", "5a", "31", "37", "f6", "30", "98", "07", "a8", "8d", "a2", "34"]:
+    message += chr(int(i, 16))
+key = ""
+for i in ["2b", "28", "ab", "09", "7e", "ae", "f7", "cf", "15", "d2", "15", "4f", "16", "a6", "88", "3c"]:
+    key += chr(int(i, 16))
+
+print(message)
+print(key)
+
+aes = AesEncryption(key, 8)
+e_message = aes.encrypt(message)
+print(e_message)
+
+
+d_message = aes.decrypt(e_message)
+print(d_message)
+
+---------
+
+aes = AesEncryption("aesEncryptionKey", 16)
+message = "hello world"
+print(message)
+e_message = aes.encrypt(message)
+print(e_message)
+d_message = aes.decrypt(e_message)
+print(d_message)
+
+
 """
