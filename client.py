b51e89499d5d32ba66b683973f38b0f416110bcf
Xavier Tondelier <xtondelier@gmail.com>
2025-11-21 13:38:27 +0000

    Add basic client and server files
    They do have a few errors regarding the validation of a key and the
    receiving of messages. This work while I was school so that is why there
    is a lack of separate commits.

---
+++ client.py
@@ -0,0 +1,62 @@
+import socket, random
+
+aes = __import__("aes-encryption")
+rsa = __import__("rsa-encryption")
+
+HEADERSIZE = 5
+
+def format_message(message, header_size):
+    bytes_message = str(message).encode(encoding="utf-8")
+    return f"{len(bytes_message):<{header_size}}".encode(encoding="utf-8") + bytes_message
+
+class Client:
+    def __init__(self, ip, port, bits_per_prime=256):
+        self.header_size = 5
+
+        self.generate_rsa_key_pair(bits_per_prime)
+
+        self.connect_socket(ip, port)
+
+    def connect_socket(self, ip, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.sock.connect((ip, port))
+
+        self.sock.send(format_message(self.pub.e, self.header_size) + format_message(self.pub.n, self.header_size))
+
+        conn_e = int(self.receive_message(False))
+        conn_n = int(self.receive_message(False))
+        aes_key = self.priv.decrypt(self.receive_message(False))
+        print("ak:      ",aes_key)
+
+        self.aesEncryption = aes.AesEncryption(aes_key, 8)
+        self.sock_pub = rsa.RsaPublic(conn_e, conn_n)
+
+        received_msg = self.aesEncryption.encrypt("aes key received")
+        self.sock.send(format_message(received_msg, self.header_size))
+
+    def generate_rsa_key_pair(self, bits):
+        rsaEncryption = rsa.RsaEncryption(8)
+        print("Generating keys ... ", end="", flush=True)
+        rsaEncryption.generate_keys(bits)
+        print("done\n")
+        [self.pub, self.priv] = rsaEncryption.generate_classes()
+
+    def receive_message(self, aes_decrypt=True):
+        length = int(self.sock.recv(self.header_size).decode(encoding="utf-8").strip())
+        if aes_decrypt:
+            return self.aesEncryption.decrypt(self.sock.recv(length).decode(encoding="utf-8"))
+        else:
+            return self.sock.recv(length).decode(encoding="utf-8")
+    
+
+    def send_message(self, message):
+        e_message = self.aesEncryption.encrypt(message)
+        self.sock.send(format_message(e_message, self.header_size))
+
+def main():
+    client = Client("127.0.0.1", 2800, 256)
+    client.sock.close()
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,105 @@
+import socket, random
+from select import select
+aes = __import__("aes-encryption")
+rsa = __import__("rsa-encryption")
+
+class Server:
+    def __init__(self, ip, port, bits_per_prime=256):
+        self.header_size = 5
+
+        self.generate_rsa_key_pair(bits_per_prime)
+
+        self.initiate_socket(ip, port)
+
+    def initiate_socket(self, ip, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.sock.bind((ip, port))
+        self.sock.listen(5)
+
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.conns = [self.sock]
+        self.conn_information = {}
+
+    def generate_rsa_key_pair(self, bits):
+        rsaEncryption = rsa.RsaEncryption(8)
+        print("Generating keys ... ", end="", flush=True)
+        rsaEncryption.generate_keys(bits)
+        print("done\n")
+        [self.pub, self.priv] = rsaEncryption.generate_classes()
+
+    def generate_aes_key(self):
+        key = ""
+        for i in range(16):
+            key += chr(random.randint(0, 255))
+
+        return key
+
+
+    def accept_connection(self):
+        conn, addr = self.sock.accept()
+        self.conns.append(conn)
+        print(conn, addr)
+
+        conn_e = int(self.receive_message(conn, False))
+        conn_n = int(self.receive_message(conn, False))
+        aes_key = self.generate_aes_key()
+
+        aes_obj = aes.AesEncryption(aes_key, 8)
+        rsa_obj = rsa.RsaPublic(conn_n, conn_e, 8)
+        self.conn_information[conn] = {"rsa": rsa_obj,
+                                       "aes": aes_obj }
+        print("ak:     ",aes_key)
+        conn.send(format_message(self.pub.e, self.header_size) + format_message(self.pub.n, self.header_size))
+        conn.send(format_message(rsa_obj.encrypt(aes_key), self.header_size))
+
+        valid_received_msg = "aes key received"
+        received_msg = self.receive_message(conn)
+        print(received_msg)
+        if received_msg != valid_received_msg:
+            self.remove_conn(conn)
+            
+
+    def remove_conn(self, conn):
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
+        self.conns.remove(conn)
+        self.conn_information.pop(conn)
+
+    def receive_message(self, conn, aes_decrypt=True):
+        length = int(conn.recv(self.header_size).decode(encoding="utf-8").strip())
+        if length == "":
+            return False
+
+        if aes_decrypt:
+            return self.conn_information[conn]["aes"].decrypt(conn.recv(length).decode(encoding="utf-8"))
+        else:
+            return conn.recv(length).decode(encoding="utf-8")
+
+    def distribute_message(self, conn, message):
+        message = self.conn_information[conn]["aes"].decrypt(message.decode(encoding="utf-8"))
+        print(conn, message)
+        for other_conn in self.conns:
+            if other_conn != conn:
+                e_message = self.conn_information[other_conn]["aes"].encrypt(message)
+                other_conn.send(format_message(e_message, self.header_size))
+
+def format_message(message, header_size):
+    bytes_message = str(message).encode(encoding="utf-8")
+    return f"{len(bytes_message):<{header_size}}".encode(encoding="utf-8") + bytes_message
+
+server = Server("0.0.0.0", 2800, 256)
+while True:
+    conns_to_read, _, conns_in_error = select(server.conns,server. conns,server. conns)
+    for conn in conns_in_error:
+        server.remove_conn(conn)
+
+    for conn in conns_to_read:
+        if conn == server.sock:
+            server.accept_connection()
+        else:
+            message = server.receive_message(conn)
+            if not message:
+                server.remove_conn(conn)
+            server.distribute_message(conn, message)
